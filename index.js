71b31968f319bb012db95556e6affeff2488c7c4
Justin Lyan <33265378+justinlyan@users.noreply.github.com>
2024-11-18 16:18:31 -0600

    operato function

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
+let operator;
+let firstNum;
+let secondNum;
+
 const add = (x, y) => {
   return x + y;
-}
+};
 
 const subtract = (x, y) => {
   return x - y;
-}
+};
 
 const multiply = (x, y) => {
   return x * y;
-}
+};
 
 const divide = (x, y) => {
   return x / y;
+};
+
+const operate = (operator, x, y) => {
+  if (operator == "add") {
+    return add(x, y);
+  } else if (operator == "subtract") {
+    return subtract(x, y);
+  } else if (operator == "multiply") {
+    return multiply(x, y);
+  } else if (operator == "divide") {
+    return divide(x, y);
+  } 
 }
