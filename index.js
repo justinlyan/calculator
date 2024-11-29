adeffb65e9065c79ac4edc687ce862636b3a30af
Justin Lyan <33265378+justinlyan@users.noreply.github.com>
2024-11-29 15:50:56 -0600

    user can now use numbers with decimals

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ let hasOperator = false;
 let firstNum = "";
 let secondNum = "";
 let onFirstNumber = true;
+let canAddDecimal = true;
+let hasDecimal = false;
 
 const display = document.querySelector(".display");
-const numbers = document.querySelectorAll(".number");
-const operators = document.querySelectorAll(".operator");
-const clear = document.querySelector(".clear");
-const equals = document.querySelector(".equals");
+const numberBtns = document.querySelectorAll(".number");
+const operatorBtns = document.querySelectorAll(".operator");
+const clearBtn = document.querySelector(".clear");
+const equalsBtn = document.querySelector(".equals");
+const decimalBtn = document.querySelector(".decimal");
+const deleteBtn = document.querySelector(".delete")
 
 const add = (x, y) => {
   return x + y;
@@ -23,6 +27,10 @@ const multiply = (x, y) => {
 };
 
 const divide = (x, y) => {
+  if (y === 0) {
+    clear();
+    return "Error";
+  }
   return x / y;
 };
 
@@ -38,23 +46,63 @@ const operate = (operator, x, y) => {
   } 
 };
 
-numbers.forEach((number) => {
+const clear = () => {
+  firstNum = "";
+  secondNum = "";
+  onFirstNumber = true;
+  hasOperator = false;
+  canAddDecimal = true;
+  hasDecimal = false;
+}
+
+numberBtns.forEach((number) => {
   number.addEventListener("click", () => {
     if (!onFirstNumber && !hasOperator && firstNum) {
       onFirstNumber = true;
       firstNum = "";
+      hasDecimal = false;
     } 
     if (onFirstNumber) {
+      if (firstNum === "0") {
+        firstNum = '';
+      }
       firstNum += number.textContent;
       display.textContent = firstNum;
     } else {
+      if (secondNum === "0") {
+        secondNum = "";
+      }
       secondNum += number.textContent;
       display.textContent = secondNum;
     }
   })
 });
 
-operators.forEach((operator) => {
+decimalBtn.addEventListener("click", () => {
+  if (canAddDecimal) {
+    if (onFirstNumber) {
+      if (!firstNum) {
+        firstNum = "0.";
+      } else {
+        firstNum += ".";
+      }
+      display.textContent = firstNum;
+      canAddDecimal = false;
+      hasDecimal = true;
+    } else {
+      if (!secondNum) {
+        secondNum = "0.";
+      } else {
+        secondNum += ".";
+      }
+      display.textContent = secondNum;
+      canAddDecimal = false;
+      hasDecimal = true;
+    }
+  }
+});
+
+operatorBtns.forEach((operator) => {
   operator.addEventListener("click", () => {
     if (firstNum && !hasOperator) {
       sign = operator.textContent;
@@ -69,24 +117,50 @@ operators.forEach((operator) => {
       secondNum = "";
       sign = operator.textContent;
     }
+
+    canAddDecimal = true;
   })
 });
 
-clear.addEventListener("click", () => {
+clearBtn.addEventListener("click", () => {
   display.textContent = "0";
-  firstNum = "";
-  secondNum = "";
-  onFirstNumber = true;
-  hasOperator = false;
+  clear();
 });
 
-equals.addEventListener("click", () => {
+equalsBtn.addEventListener("click", () => {
   if (hasOperator && firstNum && secondNum) {
-    let result = operate(sign, parseInt(firstNum), parseInt(secondNum));
+    let result;
+    if (hasDecimal) {
+      result = operate(sign, parseFloat(firstNum), parseFloat(secondNum));
+    } else {
+      result = operate(sign, parseInt(firstNum), parseInt(secondNum));
+    }
+
     firstNum = result;
     secondNum = "";
     hasOperator = false;
     onFirstNumber = false;
+    canAddDecimal = true;
     display.textContent = firstNum;
   }
-})
+});
+
+deleteBtn.addEventListener("click", () => {
+  if (onFirstNumber && firstNum) {
+    let firstNumArray = firstNum.split('');
+    firstNumArray.pop();
+    firstNum = firstNumArray.join('');
+    if (!firstNum) {
+      firstNum = "0";
+    }
+    display.textContent = firstNum;
+  } else if (!onFirstNumber && secondNum) {
+    let secondNumArray = secondNum.split('');
+    secondNumArray.pop();
+    secondNum = secondNumArray.join('');
+    if (!secondNum) {
+      secondNum = "0";
+    }
+    display.textContent = secondNum;
+  }
+});
